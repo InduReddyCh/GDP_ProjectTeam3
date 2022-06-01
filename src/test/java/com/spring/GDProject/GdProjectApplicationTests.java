28ed322482ac0bbd31ef017689d237bb5967842c
HimajaReddyMaddi <S545156@nwmissouri.edu>
2022-05-31 20:08:38 -0500

    Adding project

---
+++ src/test/java/com/spring/GDProject/GdProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.GDProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
